c017507c3aca68158e6a810f3aa30065a9d9328f
jul1alcaraz <lic.alcarazaballay2022@gmail.com>
2025-11-01 13:21:17 -0300

    CORS fixed

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const dotenv = require('dotenv');
 
 const app = express();
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  express.json();
+  next();
+});
 
 conectarDB();
 

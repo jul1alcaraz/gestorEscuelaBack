b5beea7e735b314a99cb8b08fcdba0e01015a768
jul1alcaraz <lic.alcarazaballay2022@gmail.com>
2025-11-04 23:13:28 -0300

    CORS new config

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   express.json();
   next();
 });
